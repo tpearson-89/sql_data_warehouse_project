deae10d6f94cced4f965aaa983909bc1aa22fe1a
tpearson-89 <thomas.pearson89@gmail.com>
2025-04-21 17:17:06 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,25 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseProject1'. 
+    Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    If the database, or schemas, already exist, these will need to be dropped first.
+*/
 
+-- Create the 'DataWarehouseProject1' database
+CREATE DATABASE DataWarehouseProject1;
+
+-- Select the created database
+USE DataWarehouse;
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
